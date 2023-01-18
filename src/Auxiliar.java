ab9779fd70a80ba844f0d9c91179a1b37b6b9d46
cdavi <cdavi13@gmail.com>
2023-01-18 16:59:21 -0300

    Project Euler Problem 002(resolved)

--- src/Auxiliar.java
+++ src/Auxiliar.java
@@ -1,15 +1,30 @@
 public class Auxiliar {
 
+    private long results;
     Auxiliar(){
     }
+    public boolean calc() {
 
-    public int calc(int num){
-        if(num % 5 == 0){
-            return num;
-        }else if (num % 3 == 0){
-            return num;
+        int termo1 = 1;
+        int termo2 = 1;
+        int i = 1;
+        int resultado = 0;
+        while (termo2 + termo1 < 4000000) {
+            resultado = termo1 + termo2;
+            termo1 = termo2;
+            termo2 = resultado;
+            i++;
+            System.out.println(i);
+
+            if (resultado % 2 == 0) {
+                this.results = this.results + resultado;
+            }
+
         }
-        return 0;
-    }
 
+        return true;
+    }
+    public long getResult() {
+        return results;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,10 @@ public class Main {
     public static void main(String[] args) {
 
         Auxiliar auxiliar = new Auxiliar();
-        int num = 0;
-       int result = 0;
-        while (num < 1000){
 
-        result = result + auxiliar.calc(num);
+        boolean result = false;
+        result = auxiliar.calc();
 
-            System.out.println("N:" + num);
-            System.out.println("R: " + result);
-        num+=1;
-
-
-
-
-
-        }
-        System.out.println("Final Result: " + result);
+        System.out.println("Final Result: " + auxiliar.getResult());
     }
 }
