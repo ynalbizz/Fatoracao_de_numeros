84e16b491fe85a3b646f05c3eca43788efd88c14
cdavi <cdavi13@gmail.com>
2023-01-18 23:19:54 -0300

    Fatoração de numeros :P Written by: myself Reviewed-by: nobody
    Version 1.0

--- src/Auxiliar.java
+++ src/Auxiliar.java
@@ -1,2 +1,63 @@
+import java.util.ArrayList;
+import java.util.List;
 public class Auxiliar {
+    List<Integer> fatores = new ArrayList<>();
+    List<Integer> indices = new ArrayList<>();
+
+    String[] resultArr;
+
+    Auxiliar(){
+    }
+    public void calc(long x) {
+
+        int n = 2;
+        while (x != 1) {
+
+
+            while (x % n != 0) {
+                n++;
+            }
+
+            int indiceAtual = 0;
+            while (x % n == 0) {
+                x = x / n;
+                indiceAtual += 1;
+            }
+            this.fatores.add(n);
+            n++;
+            this.indices.add(indiceAtual);
+
+        }
+    }
+
+/*
+    public List getFatores() {
+        return fatores;
+    }
+
+    public List getIndices() {
+        return indices;
+    }
+*/
+    public String[] getResultArr(){
+        return resultArr;
+    }
+
+    public void getResults() {
+        int p = indices.size();
+        List<String> result = new ArrayList<>();
+
+        for (int i = 0; i < p ; i++){
+            result.add("  " + fatores.get(i) + "^" + indices.get(i) + "  ");
+
+        }
+
+        int n = result.size(); //fetching list size
+        String[] resultArr = new String[n]; //new int array of given size
+        for(int i=0;i<n;i++){ //looping over each element and adding them to array
+            resultArr[i] = result.get(i);
+        }
+        this.resultArr = resultArr;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import javax.swing.*;
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Auxiliar aux = new Auxiliar();
+
+        String numString = JOptionPane.showInputDialog("title");
+        long num = Integer.parseInt(numString);
+        //long num = 13195;
+        aux.calc(num);
+        aux.getResults();
+        String[] result = aux.getResultArr();
+        JOptionPane.showMessageDialog(null, Arrays.toString(result));
+
+
     }
 }
